5c992f3bce97b0b231bd93a16043987e879d0771
SamMasca7950 <160941893+SamMasca7950@users.noreply.github.com>
2024-02-22 20:59:43 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
-import pickle
+import joblib
 
-# Assuming your preprocessing steps are applicable to new data inputs
+
 def preprocess_data(input_df):
     outlier_columns = ['citric acid', 'residual sugar', 'free sulfur dioxide', 'density']
     for col in outlier_columns:
@@ -12,15 +12,14 @@ def preprocess_data(input_df):
         z_scores = stats.zscore(input_df[col])
         outliers = np.abs(z_scores) > 4
         input_df.loc[outliers, col] = col_mean
-    # Include other preprocessing steps as needed
+    
     return input_df
 
 def predict(input_features):
-    # Load the model
-    with open('model.pkl', 'rb') as file:
-        model = pickle.load(file)
-    # Preprocess input features
+    
+    with open('lm_model_prediction.sav', 'rb') as file:
+        model = joblib.load(file)
+
     preprocessed_features = preprocess_data(input_features)
-    # Make prediction
     prediction = model.predict(preprocessed_features)
     return prediction
